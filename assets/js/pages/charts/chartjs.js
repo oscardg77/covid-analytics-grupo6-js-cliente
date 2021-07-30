0ee742ec619d2b25f2477091e9c7205de32fe261
oscardg77 <82647316+oscardg77@users.noreply.github.com>
2021-07-30 21:11:11 +0200

    subi js-cliente

---
+++ assets/js/pages/charts/chartjs.js
@@ -0,0 +1,119 @@
+import { listadoSumEntries } from '../../script.js';
+
+$(function () {
+     new Chart(document.getElementById("line_chart").getContext("2d"), getChartJs('line'));
+     new Chart(document.getElementById("bar_chart").getContext("2d"), getChartJs('bar'));
+     new Chart(document.getElementById("radar_chart").getContext("2d"), getChartJs('radar'));
+     new Chart(document.getElementById("pie_chart").getContext("2d"), getChartJs('pie'));
+});
+
+function getChartJs(type) {
+    var config = null;
+
+    if (type === 'line') {
+        config = {
+            type: 'line',
+            data: {
+                labels: ["spain", "argelia", "germany", "finland", "argentina" ], 
+                datasets: [{
+                    label: "Entries",
+                    data: [196, 1 , 165, 162, 58],
+                    borderColor: 'rgba(0, 188, 212, 0.75)',
+                    backgroundColor: 'rgba(0, 188, 212, 0.3)',
+                    pointBorderColor: 'rgba(0, 188, 212, 0)',
+                    pointBackgroundColor: 'rgba(0, 188, 212, 0.9)',
+                    pointBorderWidth: 1
+                }, {
+                        label: "Countries",
+                        data: [28, 48, 40, 19, 86, 27, 90],
+                        borderColor: 'rgba(233, 30, 99, 0.75)',
+                        backgroundColor: 'rgba(233, 30, 99, 0.3)',
+                        pointBorderColor: 'rgba(233, 30, 99, 0)',
+                        pointBackgroundColor: 'rgba(233, 30, 99, 0.9)',
+                        pointBorderWidth: 1
+                    }]
+            },
+            options: {
+                responsive: true,
+                legend: false
+            }
+        }
+    }
+    else if (type === 'bar') {
+        config = {
+            type: 'bar',
+            data: {
+                labels: ["Bahamas", "Ecuador", "China", "Japan", "palentine"],
+                datasets: [{
+                    label: "Entries",
+                    data: [60, 77, 111, 117, 131],
+                    backgroundColor: 'rgba(0, 188, 212, 0.8)'
+                }, {
+                        label: "Countries",
+                        data: [28, 48, 40, 19, 86, 27, 90],
+                        backgroundColor: 'rgba(233, 30, 99, 0.8)'
+                    }]
+            },
+            options: {
+                responsive: true,
+                legend: false
+            }
+        }
+    }
+    else if (type === 'radar') {
+        config = {
+            type: 'radar',
+            data: {
+                labels: ["Yemen", "Belgium", "Monaco", "poland", "Sweden"],
+                datasets: [{
+                    label: "entries",
+                    data: [146, 153, 183, 188, 197],
+                    borderColor: 'rgba(0, 188, 212, 0.8)',
+                    backgroundColor: 'rgba(0, 188, 212, 0.5)',
+                    pointBorderColor: 'rgba(0, 188, 212, 0)',
+                    pointBackgroundColor: 'rgba(0, 188, 212, 0.8)',
+                    pointBorderWidth: 1
+                }, {
+                        label: "countries",
+                        data: [72, 48, 40, 19, 96, 27, 100],
+                        borderColor: 'rgba(233, 30, 99, 0.8)',
+                        backgroundColor: 'rgba(233, 30, 99, 0.5)',
+                        pointBorderColor: 'rgba(233, 30, 99, 0)',
+                        pointBackgroundColor: 'rgba(233, 30, 99, 0.8)',
+                        pointBorderWidth: 1
+                    }]
+            },
+            options: {
+                responsive: true,
+                legend: false
+            }
+        }
+    }
+    else if (type === 'pie') {
+        config = {
+            type: 'pie',
+            data: {
+                datasets: [{
+                    data: [225, 50, 100, 40],
+                    backgroundColor: [
+                        "rgb(233, 30, 99)",
+                        "rgb(255, 193, 7)",
+                        "rgb(0, 188, 212)",
+                        "rgb(139, 195, 74)"
+                    ],
+                }],
+                labels: [
+                    "Pink",
+                    "Amber",
+                    "Cyan",
+                    "Light Green"
+                ]
+            },
+            options: {
+                responsive: true,
+                legend: false
+            }
+        }
+    }
+    return config;
+}
---
+++ assets/js/script.js
@@ -0,0 +1,76 @@
+﻿const hostName = 'http://https://covid-analytics-grupo6-api-laravel.test';
+// Login / Logout
+async function login(data = {}) {
+  return await apiCall('POST', hostName + '/api/login', null, data);
+};
+
+async function logout() {
+  return await apiCall('POST', hostName + '/api/logout',  null);
+};
+
+// Regions
+async function listadoSumEntries() {
+  return await apiCall('GET', hostName + '/api/entries', null, null);
+};
+
+async function crearRegions( data = {}) {
+  return await apiCall('POST', hostName + '/api/regions', data);
+};
+
+async function getNumRegionsHoy() {
+  return await apiCall('POST', hostName + '/api/numeroRegionsHoy',  null);
+};
+
+// Countries
+async function listadoCountries() {
+  return await apiCall('GET', hostName + '/api/listadoCountries',  null);
+};
+
+async function crearCountries(data={}) {
+  return await apiCall('POST', hostName + '/api/crearCountries',  data);
+};
+
+async function marcarCountries( data, countriesId) {
+  return await apiCall('PUT', hostName + '/api/marcarCountries/' + countriesId, data);
+};
+
+async function borrarCountries( countriesId) {
+  return await apiCall('DELETE', hostName + '/api/borrar/' + countriesId, null);
+};
+
+// Entries
+async function listadoEntries() {
+  return await apiCall('GET', hostName + '/api/Entries',  null);
+};
+
+async function crearEntries( data={}) {
+  return await apiCall('POST', hostName + '/api/crearEntries', token, data);
+};
+
+async function marcarEntries( data={}, entriesId) {
+  return await apiCall('PUT', hostName + '/api/marcarEntries/' + entriesId, data);
+};
+
+async function borrarEntries( EntriesId) {
+  return await apiCall('DELETE', hostName + '/api/borrarEntries/' + entriesId,  null);
+};
+// Datas
+async function listadoDatas() {
+  return await apiCall('GET', hostName + '/api/listadoDatas', null);
+};
+
+const apiCall = async (method, url) => {
+  let config = {
+    method: method,
+    url: url,
+  }
+
+
+  const response = await axios(config);
+
+  return response.data;
+}
+
+export {
+  listadoSumEntries
+}
